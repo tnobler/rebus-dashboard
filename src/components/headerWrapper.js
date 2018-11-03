e9ddc593447eaf821d7d3abaeb3d5d47c716c3fb
Tanner Noble <tannernoble@gmail.com>
2018-11-02 23:19:51 -0500

    built base grid

--- src/components/headerWrapper.js
+++ src/components/headerWrapper.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import HeaderBar from './headerBar';
 
 class HeaderWrapper extends Component {
     render () {
-        return (
-        <div className='header-wrapper'>
-            <h1 className='header-wrapper__title'>Welcome to Rebus Dashboard!</h1>
-            <p className='header-wrapper__subtitle'>Please login to continue</p>
-            {this.props.children}
-        </div>
-        );
+        return [
+            <div className='header-wrapper'>
+                <h1 className='header-wrapper__title'>Welcome to Rebus Dashboard!</h1>
+                <p className='header-wrapper__subtitle'>Please login to continue</p>
+                
+            </div>,
+            HeaderBar(),
+            this.props.children
+        ];
     }
 }
 
