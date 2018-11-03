3d947296ddf28d47b20a0b3695bd2c5981204cf8
Tanner Noble <tannernoble@gmail.com>
2018-11-03 14:38:28 -0500

    styled form fields

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -5,14 +5,29 @@ import { FormTitle } from '../formTitle';
 import { FormInput } from '../formFields';
 
 class SigninForm extends Component {
-  render () {
-    return (
-      <form className='sign-in-form'>
-          <FormTitle className='sign-in-form__title' text='Login'/>
-          <Field className='sign-in-form__email' component={FormInput} name='email' type='email'/>
-      </form>
-    );
-  }
+    render() {
+        return (
+            <form className='sign-in-form'>
+                <FormTitle className='sign-in-form__title' text='Login' />
+                <Field 
+                    className='sign-in-form__email'
+                    placeholder='Enter Email'
+                    name='email'
+                    type='email'
+                    title='Email' 
+                    component={FormInput}
+                />
+                <Field 
+                    className='sign-in-form__password'
+                    placeholder='Enter Password'
+                    name='password'
+                    type='password'
+                    title='Password' 
+                    component={FormInput}
+                />
+            </form>
+        );
+    }
 }
 
 SigninForm = reduxForm({
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 
 
 export class FormInput extends Component {
-      render() {
-          const { className, title, name } = this.props;
-          return (
-              <div className='form-input'>
-                <label>{title}</label>
-                <input className={`${className} form-input`}/>
-              </div>
+    render() {
+        const { className, title, input, type, placeholder } = this.props;
+        return (
+            <div className='form-input'>
+                <label className='form-input__title'>{title}</label>
+                <input
+                    className={`${className} form-input__input`}
+                    type={type}
+                    {...input}
+                    placeholder={placeholder}
+                />
+            </div>
         )
     }
 }
